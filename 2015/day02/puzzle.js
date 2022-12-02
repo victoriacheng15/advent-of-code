3e7a7221ad6b7cd2d68929722c8f489f4e735286
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-12-02 14:42:08 -0700

    2022 02 (#3)
    * feat: add day 2 solution and input
    
    * refactor: split solution to part1 and part2 functions

--- 2015/day02/puzzle.js
+++ 2015/day02/puzzle.js
@@ -1,39 +1,42 @@
 const fs = require("fs/promises");
 const path = require("path");
 
-const getTotalWrapper = async () => {
+function part1(input) {
+	const wrapper = input.reduce((total, _lwh) => {
+		const [l, w, h] = _lwh.split("x");
+
+		return (
+			total + 2 * l * w + 2 * w * h + 2 * h * l + Math.min(l * w, w * h, h * l)
+		);
+	}, 0);
+
+	console.log(`Total wrapper needed: ${wrapper}`);
+}
+
+function part2(input) {
+	const ribbon = input.reduce((total, lwh) => {
+		const [l, w, h] = lwh
+			.split("x")
+			.map(Number)
+			.sort((a, b) => a - b);
+
+		return total + (l + l + w + w) + l * w * h;
+	}, 0);
+
+	console.log(`Total ribbon needed: ${ribbon}`);
+}
+
+const day02 = async () => {
 	try {
 		const file = path.join(__dirname, "input.txt");
-		const data = await (
-			await fs.readFile(file, { encoding: "utf-8" })
-		).split("\n");
+		const data = await await fs.readFile(file, { encoding: "utf-8" });
+		const input = data.split("\n");
 
-		// part 1
-		const wrapper = data.reduce((total, _lwh) => {
-			const [l, w, h] = _lwh.split("x");
-
-			return (
-				total +
-				2 * l * w +
-				2 * w * h +
-				2 * h * l +
-				Math.min(l * w, w * h, h * l)
-			);
-		}, 0);
-		console.log(`Total wrapper needed: ${wrapper}`);
-
-		// part 2
-
-		const ribbon = data.reduce((total, lwh) => {
-			const [l, w, h] = lwh.split("x").map(Number).sort((a, b) => a - b);
-
-			return total + (l + l + w + w) + l * w * h;
-		}, 0);
-
-		console.log(`Total ribbon needed: ${ribbon}`);
+		part1(input);
+		part2(input);
 	} catch (error) {
 		console.log(error);
 	}
 };
 
-getTotalWrapper();
+day02();
