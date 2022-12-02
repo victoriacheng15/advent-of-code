cb5ac6d18ed5af352981f1bacadda9c59aba10fc
Victoria Cheng <victoria.cheng15@gmail.com>
2022-12-02 00:15:52 -0700

    change folder name for 2022

--- 2022/day01/puzzle.js
+++ 2022/day01/puzzle.js
@@ -10,20 +10,18 @@ function getElfTotalCalories(array) {
 const getCalories = async () => {
 	try {
 		const file = path.join(__dirname, "input.txt");
-		const data = await fs.readFile(file, {
-			encoding: "utf8",
-		});
+		const data = await fs.readFile(file, { encoding: "utf-8" });
 		const elves = data.split("\n\n");
+		
+		// part 1
 		const totalElvesCalories = getElfTotalCalories(elves);
 		const sortedCalories = totalElvesCalories.sort((a, b) => b - a);
-
-		// part 1
-		console.log("highest calories count: ", sortedCalories[0]);
+		console.log(`highest calories count: ${sortedCalories[0]} `);
 
 		//part 2
 		const top3 = sortedCalories.slice(0, 3);
 		const totalTop3Calories = top3.reduce((acc, cal) => acc + cal, 0);
-		console.log("Total top 3 elves calories count: ", totalTop3Calories);
+		console.log(`Total top 3 elves calories count: ${totalTop3Calories}`);
 	} catch (error) {
 		console.log(error);
 	}
