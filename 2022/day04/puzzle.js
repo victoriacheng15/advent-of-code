1848d7b7c6951c2954d19eacad6bb5ad11578159
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-12-04 13:32:22 -0700

    2022 04 (#5)
    * feat: add 2022 04 solutions and input 🎄
    
    * style: run formatter

---
+++ 2022/day04/puzzle.js
@@ -0,0 +1,45 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const regex = /\D/g;
+
+function part1(input) {
+	const result = input
+		.map((line) => line.split(regex).map(Number))
+		.filter(
+			([l1, l2, r1, r2]) => (l1 >= r1 && l2 <= r2) || (l1 <= r1 && l2 >= r2),
+		).length;
+
+	console.log(`The amount of assignment pairs: ${result}`);
+}
+
+function part2(input) {
+	const result = input
+		.map((line) => line.split(regex).map(Number))
+		.filter(([l1, l2, r1, r2]) => {
+			return (
+        (l2 >= r1 && l2 <= r2) ||
+        (l1 >= r1 && l1 <= r2) ||
+        (r1 >= l1 && r1 <= l2) ||
+        (r2 >= l1 && r2 <= l2)
+			);
+		}).length;
+
+	// console.log({ result });
+	console.log(`The amount of overlapped assignment pairs: ${result}`)
+}
+
+const day4 = async () => {
+	try {
+		const file = path.join(__dirname, "input.txt");
+		const data = await fs.readFile(file, { encoding: "utf-8" });
+		const input = data.split("\n");
+
+		part1(input);
+		part2(input);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+day4();
