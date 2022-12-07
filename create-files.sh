2f6d44049ad454e0841c41f4e6bc9da98fe3db30
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-12-07 09:41:47 -0700

    feat: update bash script for create folders and files (#7)

--- create-files.sh
+++ create-files.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
-mkdir day-{1..25} && touch day-{1..25}/input.txt day-{1..25}/puzzle.js
+# Get the current year
+year=$(date +"%Y")
+
+# Create a folder named "year"
+mkdir $year
+
+# Change the current directory to the newly-created "year" folder
+cd $year
+
+# Iterate over the numbers 1 to 25
+for i in {1..25}; do
+  # Create a folder named dayXX, where XX is the current value of $i
+  mkdir day$(printf "%02d" $i)
+
+  # Change the current directory to the newly-created folder
+  cd day$(printf "%02d" $i)
+
+  # Create a file named puzzle.js in the current directory
+  touch puzzle.js
+
+  echo "const fs = require(\"fs/promise\");" >> puzzle.js
+  echo "const path = require(\"path\");" >> puzzle.js
+  echo "function part1() {}" >> puzzle.js
+  echo "function part2() {}" >> puzzle.js
+
+  # Create a file named input.txt in the current directory
+  touch input.txt
+
+  # Return to the parent directory
+  cd ..
+done
+
+# Return to the parent directory
+cd ..
+
+
+
